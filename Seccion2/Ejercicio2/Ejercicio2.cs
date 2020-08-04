957a2b1f0f0076ca627203d1820359c2bbf58227
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-08-04 14:56:19 -0400

    Add files via upload

---
+++ Seccion2/Ejercicio2/Ejercicio2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Ejercicio2
+    {
+        /*Realice un programa que pida al usuario 10 números,
+         calcule su media y luego muestre los que están por encima 
+         de la media. */
+         static void media()
+        {
+            int[] numeros = new int[10];
+            double media = 0.0;
+            int Total = 0;
+            int c = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i+1+". Ingrese un numero: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+                Total = Total + numeros[i];
+            }
+
+            media = Total / 10;
+            Console.WriteLine("La media es: "+media);
+            Console.WriteLine();
+            Console.WriteLine("Los numeros mayores a la media son: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (numeros[i]>media)
+                {
+                    c++;
+                    Console.WriteLine(c+". "+numeros[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            media();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Seccion2/Seccion2/Ejercicio1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion2
+{
+    class Ejercicio1
+    {
+        /* Crear un programa que pida al usuario la fecha, detallando el día (1 al 31) y 
+         el mes (1 = enero, 12 = diciembre), luego muestre en pantalla el número 
+         de días que quedan hasta final de año. */
+        static void Dias()
+        {
+            int[] DiasMes = new int[12];
+            int dia = 0;
+            int mes = 0;
+            string[] Meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre"
+            ,"Diciembre"};
+            int Dias = 0;
+            int DiasRestantes = 0;
+
+            Console.Write("Ingrese el mes: ");
+            mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Ingrese el dia: ");
+            dia = int.Parse(Console.ReadLine());
+
+            if (mes<=12 && mes>0)
+            {
+                if (dia <= 31 && dia > 0) 
+                {
+                    for (int i = 1; i < mes; i++)
+                    {
+                        DiasMes[i] = DateTime.DaysInMonth(2015, i);
+                        Dias = Dias + DiasMes[i];
+                    }
+                    Dias = Dias + dia;
+                    Console.WriteLine("El total de dias transcurridos hasta el dia {0} del mes de {1} son:{2}", dia, Meses[mes - 1], Dias);
+                    Console.WriteLine();
+                    DiasRestantes = 365-Dias ;
+                    Console.WriteLine("Por lo que la cantidad de días restante es: " + DiasRestantes);
+                }
+
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            Dias();
+            Console.ReadKey();
+
+        }
+    }
+}
